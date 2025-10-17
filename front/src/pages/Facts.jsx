6fdc383f36dae43f7c29e298a699797c5dc29ec8
Victor-Voyer <victor.voyer@oclock.school>
2025-10-17 09:31:32 +0200

    add : npm router

---
+++ front/src/pages/Facts.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+
+const Facts = () => {
+    return (
+        <div>
+            <h1>Facts</h1>
+        </div>
+    )
+}
+
+export default Facts
